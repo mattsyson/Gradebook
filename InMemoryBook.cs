70424a3dd4fd1e676e00ffff39b46f77cb6f3e0e
mattsyson <matt_syson@live.co.uk>
2024-02-19 15:49:16 +0000

    Add project files.

---
+++ InMemoryBook.cs
@@ -0,0 +1,146 @@
+﻿using System.Diagnostics;
+
+namespace Gradebook
+{
+    public delegate void GradeAddedDelegate(object sender, EventArgs args);
+
+
+    public class NamedObject 
+    {
+        public NamedObject(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+
+    public interface IBook
+    {
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        string Name { get; }
+        event GradeAddedDelegate GradeAdded;
+    }
+
+    public abstract class Book : NamedObject, IBook
+    {
+        protected Book(string name) : base(name)
+        {
+        }
+
+        public abstract event GradeAddedDelegate GradeAdded;
+
+        public abstract void AddGrade(double grade);
+
+        public abstract Statistics GetStatistics();
+    }
+
+    public class InMemoryBook : Book
+    {
+        public InMemoryBook(string name) : base(name)
+        {
+            grades = new List<double>();
+            Name = name;
+        }
+
+        public void AddGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                case 'F':
+                    AddGrade(50);
+                    break;
+
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
+        public override void AddGrade(double grade)
+        {
+            if(grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+                GradeAdded?.Invoke(this, new EventArgs());
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+
+            for(var i = 0; i < grades.Count; i++) 
+            {
+                result.Add(grades[i]);
+            }
+
+            return result; 
+        }
+
+        private List<double> grades;
+    }
+
+    public class DiskBook : Book
+    {
+        public DiskBook(string name) : base(name)
+        {
+            grades = new List<double>();
+            Name = name;
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public override void AddGrade(double grade)
+        {
+            using (var writer = File.AppendText($"{Name}.txt"))
+            {
+                writer.WriteLine(grade);
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+        }
+
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+
+            using (var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = reader.ReadLine();
+                while(line != null) 
+                {
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+                
+            }
+
+            return result;
+        }
+
+        private List<double> grades;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBook book = new InMemoryBook("Matt's Grade Book");
+            book.GradeAdded += OnGradeAdded;
+            EnterGrades(book);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The book named {book.Name}");
+            Console.WriteLine($"The average grade is {stats.Average:N2}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit ");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+
+            }
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added.");
+        }
+    }
+}
